1355b88f59fb85a1366d8ee0dc478334cbdb692c
Anik Roy <136085433+EtheSonX082531@users.noreply.github.com>
2025-01-01 20:47:55 +0600

    Create Array.java

---
+++ Array.java
@@ -0,0 +1,10 @@
+public class Array {
+	public static void main(String[] args) {
+		// Datatype arrayName[]=new Datatype[size];
+		int arr[] = {1, 2, 3, 4, 5};
+		//Using enhanced for loop
+		for (int i : arr) {
+			System.out.print(i+" ");
+		}
+	}
+}
